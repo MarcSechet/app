18f0832d772fd467b9b0c58499b9dc98bcc96e32
Marc <sechet.marc.27@gmail.com>
2019-01-13 12:51:15 +0100

    ajout home controller

--- src/main/java/com/marc/HomeController.java
+++ src/main/java/com/marc/HomeController.java
@@ -1,4 +1,18 @@
 package com.marc;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @RequestMapping("/}")
+    String mainHello(@PathVariable String name) {
+        return "Hello home page" + name + "!";
+    }
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
